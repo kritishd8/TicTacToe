d518073b68173c5c09f28471f88b4a9af2505ce5
kritishd8 <dhakalkritish7@gmail.com>
2024-02-05 09:45:11 +0545

    made the board using loop

--- noughtsandcrosses.py
+++ noughtsandcrosses.py
@@ -24,14 +24,13 @@ def draw_board(board):
                     None
     """
     print('\n')
-    print('-------------')
-    print('|', board[0][0], '|', board[0][1], '|', board[0][2], '|')
-    print('-------------')
-    print('|', board[1][0], '|', board[1][1], '|', board[1][2], '|')
-    print('-------------')
-    print('|', board[2][0], '|', board[2][1], '|', board[2][2], '|')
-    print('-------------')
-    print('\n')
+    for i in range(len(board)):
+        print('  ----------- ')
+        for j in range(len(board)):
+            print(' | ', end="")
+            print(board[i][j], end='')
+        print(" | ")
+    print('  ----------- ')
 
 
 def welcome(board):
